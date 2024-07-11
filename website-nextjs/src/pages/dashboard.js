1c645cf3bd0fa24013efac5edbf99c09a5c382e7
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-11 08:18:53 +0530

    Generated: `website-nextjs/src/pages/dashboard.js`

---
+++ website-nextjs/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <p>This is where you can access all the features and enhancements of the website.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
