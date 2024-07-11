37e96227407d7e519ce44f16057c9ec0c85ad4f3
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-11 08:18:46 +0530

    Generated: `website-nextjs/src/pages/index.js`

---
+++ website-nextjs/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Next.js Website!</h1>
+      <p>This is a saas startup type website developed using Next.js.</p>
+      {/* Add your advanced components and animations here */}
+    </div>
+  );
+};
+
+export default IndexPage;
