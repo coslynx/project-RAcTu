973bff46c6ba0c9782389a5522bc86f28484c1cf
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-11 08:18:48 +0530

    Generated: `website-nextjs/src/pages/about.js`

---
+++ website-nextjs/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our saas startup website built with Next.js!</p>
+      <p>Our goal is to provide a seamless browsing experience with advanced components and animations.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
